e4352818c7725d384cd0a34ec9cb70a1ef85b2d9
Alen Chacko <the.alenchacko@gmail.com>
2025-04-14 12:05:44 +0530

    testing went wrong

---
+++ tests-jasmin/moneyTest.js
@@ -0,0 +1,16 @@
+import { formatCurrency } from "../scripts/utils/money";
+
+// describe('test suite : formatCurrency',()=>{
+//     it('converts cents into dollars',()=>{
+//         expect(formatCurrency(2095)).toEqual('20.95')
+//     })
+// })
+
+console.log('testssss')
+
+describe('dummy suite', () => {
+    it('dummy test', () => {
+      expect(true).toBe(true);
+    });
+  });
+  
